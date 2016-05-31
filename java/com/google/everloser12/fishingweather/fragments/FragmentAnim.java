55635b8dc3d36e8dee2802a3e095990002194043
Konstantin Khurs <everloser12@gmail.com>
2016-05-31 09:32:32 +0200

    old package removed

--- java/com/google/everloser12/fishingweather/fragments/FragmentAnim.java
+++
@@ -1,9 +0,0 @@
-package com.google.everloser12.fishingweather.fragments;
-
-/**
- * Created by al-ev on 11.05.2016.
- */
-public enum FragmentAnim {
-
-    NONE, RIGHT_TO_LEFT
-}
--- java/com/google/everloser12/fishingweather/rest/CallBack.java
+++
@@ -1,9 +0,0 @@
-package com.google.everloser12.fishingweather.rest;
-
-/**
- * Created by al-ev on 29.04.2016.
- */
-public interface CallBack {
-
-    void response(boolean isError);
-}
